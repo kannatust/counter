416706bb9d9c57ab1a39f6ae5102f990b543b77f
Anna <kannatust@gmail.com>
2019-02-06 16:28:19 +0200

    täiendused

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,56 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200"
+    tags: ["tag1", "tag2", "tag3"]
+    // leiab suvalise 200x200px pildi
+    //imageUrl: "https://picsum.photos/200"
   };
+
+  // kohmakam viis bindida event handlerseid 'this'-iga, kasutada juhul, kui lühem
+  // variant (st handleIncrement = () => {) ei toimi.
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
