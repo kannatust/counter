9998f6a1827ddc0b8427104b42c8761d7edf808b
Anna <kannatust@gmail.com>
2019-02-08 15:04:47 +0200

    multiple components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,62 +2,49 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
-    // leiab suvalise 200x200px pildi
-    //imageUrl: "https://picsum.photos/200"
+    value: this.props.value
   };
 
-  // kohmakam viis bindida event handlerseid 'this'-iga, kasutada juhul, kui lühem
-  // variant (st handleIncrement = () => {) ei toimi.
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
-
-  handleIncrement = () => {
-    console.log("Increment Clicked", this);
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  handleDecrement = product => {
+    console.log(product);
+    this.setState({ value: this.state.value - 1 });
+  };
 
   render() {
     return (
       <div>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <img src={this.state.imageUrl} alt="" />
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={product => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>{" "}
+        <button
+          onClick={product => this.handleDecrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Decrement
         </button>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count == 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
